bdd422d0a73343b807f2dad3ce892fa4565a36eb
apoorv <apoorv.patne10@gmail.com>
2018-12-18 20:13:26 +0530

    Minor fixes

--- Script 1 : Sending email via command line/my_script.py
+++ Script 1 : Sending email via command line/my_script.py
@@ -1,6 +1,7 @@
 import argparse
 import smtplib
 import docx
+import re
 from os import environ
 from string import Template
 from email.mime.multipart import MIMEMultipart
@@ -11,43 +12,91 @@ password = environ.get('my_password')
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("email_id", help="Email ID")
+parser.add_argument("--email_id", help="Email ID")
 parser.add_argument("subject", help="Subject", type=str)
 parser.add_argument("file_path", help="File Path")
 args = parser.parse_args()
 
-emailid = args.email_id
+if args.email_id:
+    emailid = args.email_id
 sub = args.subject
 file_path = args.file_path
 
+def is_valid_email(email):
+    my_pattern = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+    pattern = re.compile(my_pattern)
+
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
+def get_contacts(filename):
+   emails = []
+
+   with open(filename, mode='r', encoding='utf-8') as contacts_file:
+       for contact in contacts_file:
+           emails.append(contact.split()[0])
+
+   return emails
+
 def main():
 
-    doc_file = docx.Document(file_path)
-    paragraphs = doc_file.paragraphs
-    message = ''
+    doc_file = docx.Document(file_path) ##
+    paragraphs = doc_file.paragraphs ##
+    message = '' ##
 
-    for para in paragraphs:
-        message += para.text
-        message += '\n'
+    for para in paragraphs: ##
+        message += para.text ##
+        message += '\n' ##
 
-    s = smtplib.SMTP('smtp.gmail.com:587')
-    s.starttls()
-    s.login(my_address, password)
+    s = smtplib.SMTP('smtp.gmail.com:587') ##
+    s.starttls() ##
+    s.login(my_address, password) ##
 
-    msg = MIMEMultipart()
+    if args.email_id:
 
-    msg['From'] = my_address
-    msg['To'] = emailid
-    msg['Subject'] = sub
+        msg = MIMEMultipart()
 
-    msg.attach(MIMEText(message, 'plain'))
-    s.send_message(msg)
-    del msg
-    s.quit()
+        msg['From'] = my_address
+        msg['To'] = emailid
+        msg['Subject'] = sub
+
+        msg.attach(MIMEText(message, 'plain'))
+        s.send_message(msg)
+        del msg
+        s.quit()
+
+    else:
+        print("Sending emails to all contacts provided in the contacts file")
+        emails = get_contacts('contacts.txt')
+
+        for email in emails:
+
+            msg = MIMEMultipart()
+
+            msg['From'] = my_address
+            msg['To'] = email
+            msg['Subject'] = sub
+
+            msg.attach(MIMEText(message, 'plain'))
+            s.send_message(msg)
+            del msg
+
+        s.quit()
 
 
 if __name__ == '__main__':
-    main()
+    if args.email_id:
+        if is_valid_email(emailid) and type(emailid) == str and type(sub) == str and type(file_path) == str:
+            main()
+        else:
+            print("Invalid email or invalid file paths or subject")
+    else:
+        if type(sub) == str and type(file_path) == str:
+            main()
+        else:
+            print("Invalid file paths or subject")
 
 
 
